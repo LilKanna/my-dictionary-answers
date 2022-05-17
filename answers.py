ea9b3db1e9157ceb3780ec77f0dd60fc975d4939
omerOP1321 <gnr1321.op@gmail.com>
2022-05-17 11:14:17 +0300

    code

---
+++ answers.py
@@ -0,0 +1,22 @@
+#answer 1.
+mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+
+#answer 2.
+mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+
+mydict['Last Name'] = 'Viles'
+
+#answer 3.
+mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+
+mydict['Last Name'] = 'Viles'
+mydict['Hobbies'] = 'minecraft'
+
+# answer 4.
+mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+
+mydict['Last Name'] = 'Viles'
+mydict['Hobbies'] = 'minecraft'
+
+if mydict['Name'] != 'none':
+
