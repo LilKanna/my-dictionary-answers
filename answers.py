12b48870fc433841e8cde70a9dfb236c64c16897
omerOP1321 <azaloni123@gmail.com>
2022-05-17 15:29:21 +0300

    answers 1 - 13

--- answers.py
+++ answers.py
@@ -1,21 +1,200 @@
-#answer 1.
-mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+# for question 1
 
-#answer 2.
-mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
 
-mydict['Last Name'] = 'Viles'
 
-#answer 3.
-mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
+myObejct = {
+    'name' : 'Alon',
+     'age' : 57,
+    'bith date': '1975/01/05'
 
-mydict['Last Name'] = 'Viles'
-mydict['Hobbies'] = 'minecraft'
+}
 
-# answer 4.
-mydict = { 'Name': 'Alon', 'Age': '19', 'birth date': '08/12/02' }
 
-mydict['Last Name'] = 'Viles'
-mydict['Hobbies'] = 'minecraft'
 
-if mydict['Name'] != 'none'
+
+
+# for question 2
+myObejct = {
+    'name' : 'Alon',
+     'age' : 57,
+    'birth date': '1975/01/05'
+
+}
+
+myObejct['lastName'] = 'vilces'
+
+
+
+
+
+# for question 3
+
+myObejct = {
+    'name' : 'Alon',
+     'age' : 12,
+    'bith date': '1975/01/05'
+
+}
+
+myObejct['Hobbies'] =  'Drink Coffe' , 'Playing video games' , 'Watching a movie','Soccer'
+
+#for question 4
+
+def Object():
+    myObejct = {
+        'name': 'Alon',
+        'age': 12,
+        'bith date': '1975/01/05'
+
+    }
+
+ #   if  'name' in myObejct:
+    #    return True
+#
+ #   else:
+   #     return False
+
+#for question 5
+
+def Object():
+    myObejct = {
+        'name': 'Alon',
+        'age': 6,
+        'bith date': '1975/01/05'
+
+    }
+
+  #  if  myObejct['age'] == 6:
+  #      return True
+
+  #  else:
+   #     return False
+
+
+print(Object())
+
+#for question 6
+
+
+myObejct = {
+    'name': 'Alon',
+    'age': 6,
+    'bith date': '1975/01/05',
+    'equal' : 12
+}
+
+#if myObejct['age'] == myObejct['equal']:
+   # print(True)
+
+#else:
+    #print(False)
+
+#for question 7
+
+myObejct = {
+    'name': 'Alon',
+    'age': 6,
+    'bith date': '1975/01/05',
+    'equal' : 12
+}
+
+myObejct2 = {
+    'name': 'Alon',
+    'age': 6,
+    'bith date': '1975/01/05',
+    'equal' : 12
+}
+
+
+objectSet = set(myObejct)
+objectSet2 = set(myObejct2)
+
+#for i in objectSet.intersection(objectSet2):
+    #print(i)
+
+#for question 8
+
+myObejct = {
+    'name1': 'Alon',
+    'age2': 6,
+    'bith date3': '1975/01/05',
+    'equal' : 12
+}
+
+myObejct2 = {
+    'name': 'Alon',
+    'age': 6,
+    'bith date': '1975/01/05',
+    'equal' : 12
+}
+
+objectSet = set(myObejct)
+objectSet2 = set(myObejct2)
+
+#for i in objectSet.intersection(objectSet2):
+    #print(i)
+
+#for question 9
+
+#def Comparing_keys(myObejct,name):
+  #  if name in myObejct.keys():
+   #     return True
+   # else:
+   #     return False
+
+myObejct = {
+        'name': 'Alon',
+        'age2': 6,
+        'bith date3': '1975/01/05',
+        'equal': 12
+    }
+
+#for question 10
+
+#def Comparing_values(myObejct,age):
+    # if age in myObejct.values():
+      #  return True
+    #else:
+     #   return False
+
+#myObejct = {
+    #    'name': 'Alon',
+   #     'age2': 6,
+   #     'bith date3': '1975/01/05',
+  #      'equal': 12
+  #  }
+
+#for question 11
+myObejct = {
+    'name': 1,
+    'age2': 6,
+    'bith date3': 22,
+    'equal': 12
+}
+
+convertObjec=list(myObejct.items())
+convertObjec.sort()
+
+#for question 12
+
+def update_dict(myObejct,key,value):
+      myObejct[key] = value
+      return myObejct
+
+myObejct = {
+    'name': 1,
+    'age2': 6,
+    'bith date3': 22,
+    'equal': 12
+}
+
+#for question 13
+
+def build_dict_from_list(list1,list2):
+    dict = {}
+    if len(list1) == len(list2):
+        for i in range(len(list1)):
+            dict[list1[i]] = list2[i]
+        return dict
+    else:
+        return None
